b950b8473bcb08e900759bffe431b6919e9f52b5
nabin <nkbhaie2017@gmail.com>
2021-12-11 08:32:34 -0700

    node fundamental, just a setTimeout function

---
+++ nodeFunda.js
@@ -0,0 +1,11 @@
+// Promise, the .then and .catch functions
+
+// Async and syncronouse(in order execution) codes
+
+// Async fucntions are built on top of the promise
+
+console.log("This prinst first");
+setTimeout(function(){
+    console.log("This prints third, one second later")
+}, 1000);
+console.log("This prints second")
